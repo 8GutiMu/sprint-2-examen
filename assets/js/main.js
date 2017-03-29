9905b75b4331880cf15787695e2d27d67335d98c
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-29 10:09:43 -0600

    agrega lista con pendientes predifinidos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,40 @@
 function POOLista(titulo){
   this.titulo = titulo;
 
-
 }
 
 var todasLasListas=[];
+
+//va creando nuevos objetos cada vez que se llama con el botn de el inicio
 function crearLista(){
   var titulo = document.getElementById("nombreLista").value;
 
   var lista = new POOLista(titulo);
   todasLasListas.push(lista);
-  console.log(lista)
-  console.log(todasLasListas)
   mostrar(lista);
+
 
 }
 
+// muestra en pantalla todo
 function mostrar(contenido){
-  var lista = document.getElementById("listas");
-  var nuevaLista = document.createElement("section")
-  nuevaLista.innerHTML = contenido;
-  nuevaLista.appendChild(lista);
 
+  var node = document.createElement("section");
+  node.style.fontSize = "40px";
+  var textnode = document.createTextNode(contenido.titulo);
+  node.appendChild(textnode);
+  document.getElementById("listas").appendChild(node);
 
+  var listaBebe = document.createElement("ul");
+
+  crearPendientes()
+
+}
+
+function crearPendientes(){
+  var node = document.createElement("li");
+  var textnode = document.createTextNode("bren");
+  node.appendChild(textnode);
+  document.getElementsByTagName("ul")[0].appendChild(node);
 
 }
