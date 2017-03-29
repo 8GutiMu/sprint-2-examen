7b3ba94f906dc2895834b9303b8ef63bd11f0dc6
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-29 13:42:17 -0600

    intentos de borrar lista por lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//mi clase lista
+
 function POOLista(titulo){
   this.titulo = titulo;
 
@@ -6,6 +8,7 @@ function POOLista(titulo){
 var todasLasListas=[];
 
 //va creando nuevos objetos cada vez que se llama con el botn de el inicio
+
 function crearLista(){
 
   var titulo = document.getElementById("nombreLista").value;
@@ -21,60 +24,64 @@ function crearLista(){
 
 }
 
+
 // muestra en pantalla todo
 function mostrar(contenido){
   var numeroId = Date.now();
 
-  var node = document.createElement("section");
+  var node = document.createElement("section");     // crea la seccion
   node.style.fontSize = "40px";
-  node.id=numeroId+2;
+  node.id=numeroId-1;
+  console.log ("este es el "+node.id)
 
-
-  var textnode = document.createTextNode(contenido.titulo);
+  var textnode = document.createTextNode(contenido.titulo);  //pone el titulo dentro de la seccion
   node.appendChild(textnode);
   document.getElementById("listas").appendChild(node);
 
-  var listaBebe = document.createElement("ul");
-  var titulo_2 = document.createElement("h5")
-  var boton = document.createElement("button");
-  var texto = document.createElement("input");
-  var linea = document.createElement("hr");
-  var botonEditarTitulo = document.createElement("button");
-  botonEditarTitulo.innerHTML = "Editar Titulo";
-  texto.type = "text";
-  texto.id = numeroId+1;
-  listaBebe.id = numeroId;
-  titulo_2.innerHTML = "Agrega un pendiente:";
+  var listaBebe = document.createElement("ul");     //crea un elemento lista en cada seccion
+          listaBebe.id = numeroId;  // id para lista
 
-  boton.innerHTML = "Agregar Pendiente";
+  var titulo_2 = document.createElement("h5");      //crea texto (Agregar un pendiente)
+          titulo_2.innerHTML = "Agrega un pendiente:";
+
+  var boton = document.createElement("button");     //crea botn para agregar pendiente
+          boton.innerHTML = "Agregar Pendiente";
+
+  var botonearase = document.createElement("button");     //crea botn borrar lista
+                  botonearase.innerHTML = "Borrar lista";
+
+  var texto = document.createElement("input");      // crea caja de texto para agregar pendiente
+          texto.type = "text";   //tipo de input
+          texto.id = numeroId+1; // id de input
+
+  var linea = document.createElement("hr");         // crea una linea entre listas
+  var botonEditarTitulo = document.createElement("button");   // crea boton para editar el titulo de cada lista
+          botonEditarTitulo.innerHTML = "Editar Titulo";
+
+
+  //agrega elementos dentro de la lista
   boton.onclick = function(){
-    //agrega elementos dentro de la lista
+
     var id = texto.id;
     console.log(id);
 
     var pendienteTexto = document.getElementById(texto.id).value;
 
-
-    //var textnode = document.createTextNode(pendienteTexto);
-
-    if(pendienteTexto == ""){
+    if(pendienteTexto == ""){                      // comprueba que estes metiendo algo
       alert("NO estas metiendo NADAAAA!!!")
     }else{
       var node = document.createElement("LI");
       node.innerHTML = pendienteTexto;
-      var borrar = document.createElement("button");
-      borrar.innerHTML = "borrar este"
-    }
+      }
 
 
-    //node.appendChild(textnode);
     document.getElementById(id-1).appendChild(node);
     document.getElementById(texto.id).value = "";
 
-
-
   }
 
+
+  //edita el titulo
     botonEditarTitulo.onclick= function(){
       var id = node.id;
       console.log(id);
@@ -85,7 +92,13 @@ function mostrar(contenido){
     }
 
 
-  console.log(listaBebe,boton,texto,node);
+    //borra la lista
+    botonearase.onclick= function() {
+      alert("Seguro quieres Borrar???")
+      alert("Lo siento aun no pude borrar :( )")
+
+
+    }
 
   //por cada lista pone un input y botn agregar y boton borrar
   document.getElementById("listas").appendChild(listaBebe);
@@ -93,15 +106,18 @@ function mostrar(contenido){
   document.getElementById("listas").appendChild(texto);
   document.getElementById("listas").appendChild(boton);
   document.getElementById("listas").appendChild(botonEditarTitulo);
+  document.getElementById("listas").appendChild(botonearase);
   document.getElementById("listas").appendChild(linea);
 
-  document.getElementById("nombreLista").value = "";
+
+
+  document.getElementById("nombreLista").value = "";      //vacia la caja al mandarse la funcion de pedir otra lista
 
 
 
 }
 
-function borrarTodo(){
+function borrarTodo(){                                    //borra todas las lilstas y sus pendientes
   var list = document.getElementById("listas");
     while (list.hasChildNodes()) {
         list.removeChild(list.firstChild);
