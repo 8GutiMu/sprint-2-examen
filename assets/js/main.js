2c287f94fc4f671e6f2b1627be1f8cdb5f06f779
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-29 13:01:40 -0600

    editar titulos

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,8 @@ function crearLista(){
     var lista = new POOLista(titulo);
     todasLasListas.push(lista);
     mostrar(lista);
+
   }
-
-
-
 
 }
 
@@ -41,7 +39,8 @@ function mostrar(contenido){
   var boton = document.createElement("button");
   var texto = document.createElement("input");
   var linea = document.createElement("hr");
-  var botonBorrar = document.createElement("button");
+  var botonEditarTitulo = document.createElement("button");
+  botonEditarTitulo.innerHTML = "Editar Titulo";
   texto.type = "text";
   texto.id = numeroId+1;
   listaBebe.id = numeroId;
@@ -63,6 +62,8 @@ function mostrar(contenido){
     }else{
       var node = document.createElement("LI");
       node.innerHTML = pendienteTexto;
+      var borrar = document.createElement("button");
+      borrar.innerHTML = "borrar este"
     }
 
 
@@ -70,22 +71,18 @@ function mostrar(contenido){
     document.getElementById(id-1).appendChild(node);
     document.getElementById(texto.id).value = "";
 
-    }
 
-    botonBorrar.innerHTML = "Borrar Lista";
-    botonBorrar.onclick = function(){
-      var id =node.id;
+
+  }
+
+    botonEditarTitulo.onclick= function(){
+      var id = node.id;
       console.log(id);
-      var list = document.getElementById("listas");
-        while (list.hasChildNodes()) {
-            list.removeChild(list.firstChild);
-        }
-
-
+      var nuevoTitulo = prompt("Cual es el nuevo titulo? ");
+      var titu = document.getElementById(id);
+      titu.innerHTML= nuevoTitulo;
 
     }
-
-
 
 
   console.log(listaBebe,boton,texto,node);
@@ -95,8 +92,18 @@ function mostrar(contenido){
   document.getElementById("listas").appendChild(titulo_2);
   document.getElementById("listas").appendChild(texto);
   document.getElementById("listas").appendChild(boton);
-  document.getElementById("listas").appendChild(botonBorrar);
+  document.getElementById("listas").appendChild(botonEditarTitulo);
   document.getElementById("listas").appendChild(linea);
+
   document.getElementById("nombreLista").value = "";
 
+
+
+}
+
+function borrarTodo(){
+  var list = document.getElementById("listas");
+    while (list.hasChildNodes()) {
+        list.removeChild(list.firstChild);
+    }
 }
