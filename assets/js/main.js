f057ababec95abaf4f2daf648a9f0b0fa15b386c
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-03-29 09:38:27 -0600

    agregar titulos un arreglo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,24 @@ function POOLista(titulo){
 
 }
 
-
+var todasLasListas=[];
 function crearLista(){
   var titulo = document.getElementById("nombreLista").value;
-  console.log(titulo);
+
+  var lista = new POOLista(titulo);
+  todasLasListas.push(lista);
+  console.log(lista)
+  console.log(todasLasListas)
+  mostrar(lista);
+
+}
+
+function mostrar(contenido){
+  var lista = document.getElementById("listas");
+  var nuevaLista = document.createElement("section")
+  nuevaLista.innerHTML = contenido;
+  nuevaLista.appendChild(lista);
+
+
+
 }
